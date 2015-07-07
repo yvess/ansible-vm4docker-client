49da730a0fe7440108bfeeaade1aa4a9c2a678fb
Yves Serrano <y@yas.ch>
2015-07-07 17:29:49 +0200

    initial commiet

---
+++ README.md
@@ -0,0 +1,35 @@
+Role Name
+=========
+
+setup docker server for use with vm4docker
+
+Requirements
+------------
+
+ubuntu
+
+Role Variables
+--------------
+
+
+Dependencies
+------------
+
+Example Playbook
+----------------
+
+Including an example of how to use your role (for instance, with variables passed in as parameters) is always nice for users too:
+
+    - hosts: linuxdesktop
+      roles:
+         - vm4docker-server
+
+License
+-------
+
+BSD
+
+Author Information
+------------------
+
+Yves Serrano
---
+++ files/create_certs.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+mkdir ~/.vm4docker
+cd ~/.vm4docker
+/usr/local/opt/openssl/bin/openssl genrsa -aes256 -passout pass:vm4docker -out ca-key.pem 2048
+/usr/local/opt/openssl/bin/openssl req -new -x509 -days 365 -key ca-key.pem \
+  -passin pass:vm4docker -sha256 \
+  -subj '/CN=$DOCKER_HOSTIP/O=vm4docker/C=US' \
+  -out ca.pem
+/usr/local/opt/openssl/bin/openssl genrsa -out server-key.pem 2048
+/usr/local/opt/openssl/bin/openssl req -subj "/CN=$DOCKER_HOSTIP" \
+  -new -key server-key.pem -out server.csr
+echo subjectAltName = IP:$DOCKER_HOSTIP,IP:127.0.0.1 > extfile.cnf
+/usr/local/opt/openssl/bin/openssl x509 -req -days 365 -in server.csr -CA ca.pem -CAkey ca-key.pem \
+  -CAcreateserial -out server-cert.pem -passin pass:vm4docker -extfile extfile.cnf
+/usr/local/opt/openssl/bin/openssl genrsa -out key.pem 2048
+/usr/local/opt/openssl/bin/openssl req -subj '/CN=client' -new -key key.pem -out client.csr
+echo extendedKeyUsage = clientAuth > client_extfile.cnf
+/usr/local/opt/openssl/bin/openssl x509 -req -days 365 -in client.csr -CA ca.pem -CAkey ca-key.pem \
+  -CAcreateserial -out cert.pem -passin pass:vm4docker -extfile client_extfile.cnf
+
+rm -v client.csr server.csr
+chmod -v 0400 ca-key.pem key.pem server-key.pem
+chmod -v 0444 ca.pem server-cert.pem cert.pem
